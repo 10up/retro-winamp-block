f21e9eca3c129d5139ba179febe9ad2fa947797e
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-10-31 00:20:11 +0530

    add milkdrop window

--- src/frontend.js
+++ src/frontend.js
@@ -1,4 +1,5 @@
 import Webamp from 'webamp';
+import butterchurnPresets from 'butterchurn-presets';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
@@ -40,6 +41,28 @@ domReady( () => {
 				url: `https://cdn.webampskins.org/skins/${ match[ 1 ] }.wsz`,
 			};
 		}
+	}
+
+	options.__butterchurnOptions = {
+		butterchurnOpen: true,
+		importButterchurn: () => import( 'butterchurn' ),
+		getPresets: () => {
+			const presets = butterchurnPresets.getPresets();
+
+			return Object.keys( presets ).map( ( name ) => {
+				return {
+					name,
+					butterchurnPresetObject: presets[ name ]
+				};
+			} );
+		}
+	};
+
+	options.__initialWindowLayout = {
+		main: { position: { x: 0, y: 0 } },
+		equalizer: { position: { x: 0, y: 116 } },
+		playlist: { position: { x: 0, y: 232 }, size: [0, 1] },
+		milkdrop: { position: { x: 275, y: 0 }, size: [7, 9] },
 	}
 
 	// Render the player
