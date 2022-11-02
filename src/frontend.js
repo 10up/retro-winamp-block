8f65ce7053bb8d533c0978d059d64c75b8c1443b
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-11-02 12:27:00 +0530

    fix eslint

--- src/frontend.js
+++ src/frontend.js
@@ -52,18 +52,18 @@ domReady( () => {
 			return Object.keys( presets ).map( ( name ) => {
 				return {
 					name,
-					butterchurnPresetObject: presets[ name ]
+					butterchurnPresetObject: presets[ name ],
 				};
 			} );
-		}
+		},
 	};
 
 	options.__initialWindowLayout = {
 		main: { position: { x: 0, y: 0 } },
 		equalizer: { position: { x: 0, y: 116 } },
-		playlist: { position: { x: 0, y: 232 }, size: [0, 1] },
-		milkdrop: { position: { x: 275, y: 0 }, size: [7, 9] },
-	}
+		playlist: { position: { x: 0, y: 232 }, size: [ 0, 1 ] },
+		milkdrop: { position: { x: 275, y: 0 }, size: [ 7, 9 ] },
+	};
 
 	// Render the player
 	new Webamp( options ).renderWhenReady( container );
