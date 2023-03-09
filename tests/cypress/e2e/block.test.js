455a7c18da7e790afbc40e961bfda9bbfa3788a2
barneyjeffries <barney@brightwhitespace.co.uk>
2023-03-09 14:01:31 +0000

    updated cypress wp utils and used create post method

--- tests/cypress/e2e/block.test.js
+++ tests/cypress/e2e/block.test.js
@@ -13,42 +13,26 @@ describe( 'Admin can publish posts with winamp block', () => {
 		cy.uploadMedia( 'tests/cypress/fixtures/example.mp3' );
 
 		// create new page
-		cy.visit( '/wp-admin/post-new.php' );
-
-		// close the welcome guide dialog
-		//cy.get('button[aria-label="Close dialog"]').click();
-		cy.closeWelcomeGuide();
-
-		// add page title
-		cy.get( 'h1.editor-post-title' ).first().as( 'title-input' );
-		cy.get( '@title-input' ).click().type( 'Test Winamp Block' );
-
-		// insert block
-		cy.insertBlock( 'tenup/winamp-block' );
-
-		// select mp3
-		cy.get(
-			'.wp-block-tenup-winamp-block .components-button.is-tertiary'
-		).click();
-		cy.contains( '.filename div', 'example.mp3' )
-			.closest( '.thumbnail' )
-			.click();
-		cy.get( '.media-modal .media-button-select' ).click();
-
-		// publish page
-		cy.get( '.editor-post-publish-panel__toggle' ).click();
-		cy.get(
-			'.editor-post-publish-panel__header-publish-button .editor-post-publish-button__button'
-		).click();
-
-		cy.get( '.components-snackbar', { timeout: 10000 } ).should(
-			'be.visible'
-		);
-
-		// verify page contains new block
-		cy.get( 'a.components-button.components-snackbar__action' ).click();
-		cy.get( '.wp-block-audio audio' )
-			.should( 'have.attr', 'data-src' )
-			.and( 'include', 'example' );
+		cy.createPost({
+			title: 'Test Winamp Block',
+			beforeSave: () => {
+				cy.insertBlock( 'tenup/winamp-block' );
+				// select mp3
+				cy.get(
+					'.wp-block-tenup-winamp-block .components-button.is-tertiary'
+				).click();
+				cy.get('#menu-item-browse').click();
+				cy.contains( '.filename div', 'example.mp3' )
+					.closest( '.thumbnail' )
+					.click();
+				cy.get( '.media-modal .media-button-select' ).click();
+			}
+		}).then( post => {
+			cy.visit( `/wp-admin/post.php?post=${post.id}&action=edit` );
+			// verify page contains new block
+			cy.get( '.wp-block-audio audio' )
+				.should( 'have.attr', 'src' )
+				.and( 'include', 'example' );
+		});
 	} );
 } );
