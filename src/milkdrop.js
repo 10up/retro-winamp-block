72a75535e105a0596727199ae94108622368d853
Oscar Sanchez <oscar.sanchez@fueled.com>
2026-08-26 13:32:29 -0600

    Bugfix: player positioning on FE

--- src/milkdrop.js
+++ src/milkdrop.js
@@ -19,7 +19,7 @@ export const milkdropOptions = {
 		main: { position: { x: 0, y: 0 } },
 		equalizer: { position: { x: 0, y: 116 } },
 		playlist: { position: { x: 0, y: 232 }, size: [ 0, 1 ] },
-		milkdrop: { position: { x: 0, y: 348 }, size: [ 0, 9 ] },
+		milkdrop: { position: { x: 275, y: 0 }, size: [ 7, 9 ] },
 	},
 };
 
