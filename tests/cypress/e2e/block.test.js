429f0528b22096e85254797d8ee37217a246eee8
Oscar Sanchez <oscar.sanchez@fueled.com>
2026-08-26 14:10:44 -0600

    Update e2e test

--- tests/cypress/e2e/block.test.js
+++ tests/cypress/e2e/block.test.js
@@ -1,48 +1,56 @@
-Cypress.on( 'uncaught:exception', ( err, runnable ) => {
+Cypress.on( 'uncaught:exception', () => {
 	return false;
 } );
 
 describe( 'Admin can publish posts with winamp block', () => {
 	it( 'Can insert the block and publish the post', () => {
-		// login to wordpress
 		cy.login();
 
 		cy.setPermalinkStructure( '/%year%/%postname%/' );
 
-		// upload media used for the block
 		cy.uploadMedia( 'tests/cypress/fixtures/example.mp3' );
 
-		// create new page
-		cy.createPost({
+		cy.createPost( {
 			title: 'Test Winamp Block',
 			beforeSave: () => {
-				cy.get( 'body' ).then( $body => {
-					cy.insertBlock( 'tenup/winamp-block' );
-				} );
-				// select mp3
-				cy.get('.wp-block-tenup-winamp-block').contains('button', 'Media Library').click();
-				cy.get('#menu-item-browse').click();
+				cy.insertBlock( 'tenup/winamp-block' );
+
+				// Block content lives in the editor canvas iframe on WP 7.1+.
+				cy.getBlockEditor()
+					.find( '.wp-block-tenup-winamp-block' )
+					.should( 'exist' )
+					.contains( 'button', 'Media Library' )
+					.click();
+
+				// Media modal renders in the admin document.
+				cy.get( '#menu-item-browse' ).click();
 				cy.contains( '.filename div', 'example.mp3' )
 					.closest( '.thumbnail' )
 					.click();
 				cy.get( '.media-modal .media-button-select' ).click();
-			}
-		}).then( post => {
-			cy.visit( `/wp-admin/post.php?post=${post.id}&action=edit` );
-			// verify page contains new block
-			cy.get( '.wp-block-tenup-winamp-block' );
-			// toggle to 'Manage Media' view
-			cy.get( '.components-tab-button.winamp-show-media').click();
-			// Confirm the audio file is present
-			cy.get('.wp-block-audio audio')
+			},
+		} ).then( ( post ) => {
+			cy.visit( `/wp-admin/post.php?post=${ post.id }&action=edit` );
+
+			cy.getBlockEditor()
+				.find( '.wp-block-tenup-winamp-block' )
+				.should( 'exist' )
+				.click();
+
+			// Block toolbar controls render in the admin document.
+			cy.get( '.components-tab-button.winamp-show-media' ).click();
+
+			cy.getBlockEditor()
+				.find( '.wp-block-audio audio' )
 				.should( 'have.attr', 'src' )
 				.and( 'include', 'example' );
-			// toggle to 'Show Preview' view
-			cy.get( '.components-tab-button.winamp-show-preview').click();
-			// confirm webamp is showing
-			cy.get( '#webamp' )
-				.should( 'have.css', 'display', 'block' );
 
-		});
+			cy.get( '.components-tab-button.winamp-show-preview' ).click();
+
+			// Webamp mounts on document.body in the admin frame, not the canvas iframe.
+			cy.get( '#webamp', { timeout: 15000 } )
+				.should( 'be.visible' )
+				.and( 'have.css', 'display', 'block' );
+		} );
 	} );
 } );
